78e7f1f984d61ef911d570cda924a5c5db579f68
bhaskar <bhaskarnar14@gmail.com>
2021-10-01 01:01:20 +0530

    cart counter bug fix

--- ecomm/static/ecomm/main.js
+++ ecomm/static/ecomm/main.js
@@ -1,9 +1,5 @@
 function updateCartBadge() {
-<<<<<<< HEAD
-	var elements = document.getElementsByClassName('cart-quantity-badge')
-=======
 	
->>>>>>> dev
 	url = '/api/getcartquantity'
 	fetch(url, {
 		method: 'GET',
@@ -12,14 +8,15 @@ function updateCartBadge() {
 			'X-CSRFToken': csrftoken
 		},
 	}).then(response => {
-		return response.json();
-	}).then(data => {
-		for (var element of elements) {
-			if (parseInt(data.data) > 99){
-				element.innerHTML = '99+'
-			} else{
-				element.innerHTML = data.data
-			}
+		const element = document.getElementById("cart-quantity-badge")
+		if (response){
+			response.json().then(data => {
+				if (parseInt(data.data) > 99){
+					element.innerHTML = '99' + '+'
+				} else{
+					element.innerHTML = data.data
+				}
+			})
 		}
 	})
 }
