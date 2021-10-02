c55e5b3994286d1af1ee33140fc8a6a049e6825a
bhaskar <bhaskarnar14@gmail.com>
2021-10-03 02:28:23 +0530

    pagination added

--- ecomm/views.py
+++ ecomm/views.py
@@ -3,41 +3,47 @@ from django.shortcuts import redirect, render
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView, DetailView, View
-from ecomm.models import Cart, CartItem, OrderItem, Product, Order, Review
-from django.db.models import Avg, Q
+from django.views.generic import ListView, DetailView
+from ecomm.models import Cart, CartItem, Product, Order, Review
+from django.db.models import Q
 from django.contrib import messages
 from ecomm.forms import ReviewForm
-
+from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
-    query = request.GET.get('query')
     messages.info(request,'This site is currently in development')
-    return render(request, 'ecomm/index.html',{'query': query})
+    return redirect('search-result')
 
 
 class SearchView(ListView):
     model = Product
-    paginate_by = 2
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        page_number = self.request.GET.get('page')
         query = self.request.GET.get('query') if not (self.request.GET.get('query') == '') else None
         category = self.request.GET.get('category')
-        context['query'] = query if query else "" 
+        context['query'] = query if query else ""
+        # if page_number:
+        #     context['current_query'] = self.request.GET.dict().pop('page')
+        
         if category and query:
-            context['products'] = Product.objects.filter(
+            products = Product.objects.filter(
                 Q(description__icontains=query) |
                 Q(title__icontains=query) & 
                 Q(category__icontains=category)
             )
+            paginator = Paginator(products, 8)
+            context['products'] = paginator.get_page(page_number)
         elif (query):
-            context['products'] = Product.objects.filter(
-            Q(description__icontains=query) |
-            Q(title__icontains=query)
-        )
+            paginator = Paginator(Product.objects.filter(
+                Q(description__icontains=query) |
+                Q(title__icontains=query)
+            ), 8)
+            context['products'] = paginator.get_page(page_number)
         else:
-            context['products'] = Product.objects.all()
+            paginator = Paginator(Product.objects.all(), 8)
+            context['products'] = paginator.get_page(page_number)
         
         return context
 
@@ -62,8 +68,6 @@ def productDetailView(request, sku):
 
     form = ReviewForm()
     reviews = Review.objects.filter(product=model.id).all().order_by('-review_date')
-    # avg_rating = reviews.aggregate(Avg('rating')).get('rating__avg') # calculates avarage rating
-    # rating= avg_rating if avg_rating else None 
 
     return render(request, 'ecomm/product-details.html', {'form': form, 'reviews': reviews, 'object': model })
 
