b26d0bce9d1a8bb24ad3437eca6525f2957eb9e6
bhaskar <bhaskarnar14@gmail.com>
2021-10-04 02:00:21 +0530

    feature update: review page

--- ecomm/urls.py
+++ ecomm/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from ecomm.views import SearchView, productDetailView, OrderListView, cart
+from ecomm.views import SearchView, productDetailView, OrderListView, cart, ReviewListView
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('search', SearchView.as_view(template_name = 'ecomm/search-result.html'), name='search-result'),
     path('products/<sku>', productDetailView, name='product-details'),
+    path('products/<sku>/reviews', ReviewListView.as_view(template_name='ecomm/reviews.html'), name='reviews'),
     path('orders', OrderListView.as_view(template_name='ecomm/myorders.html'), name='myorder'),
     path('cart', cart, name='cart'),
     path('orders/<pk>', views.OrderDetailView.as_view(template_name='ecomm/order-details.html'), name='order-details')
